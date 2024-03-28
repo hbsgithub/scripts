d15d1ad6611f5c92b47242d20e1fcf08981021f6
HB <64822529+hbsgithub@users.noreply.github.com>
2024-03-28 12:48:32 +0800

    Update net-lsp-beta.js

--- surge/modules/network-info/net-lsp-beta.js
+++ surge/modules/network-info/net-lsp-beta.js
@@ -37,7 +37,7 @@ if (isRequest()) {
 }
 
 $.lodash_set(arg, 'IPv6', 1)
-$.lodash_set(arg, 'LANDING_IPv4', "ipsb")
+//$.lodash_set(arg, 'LANDING_IPv4', "ipsb")
 $.lodash_set(arg, 'LANDING_IPv6', "ipsb")
 const keya = 'spe'
 const keyb = 'ge'
